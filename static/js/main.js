8887184492ca924ebd30468fcdf6531b64cae996
Tim Erickson <tim@neutyp.com>
2014-12-17 17:15:25 -0800

    tweak times— need to make this accurate to real daylight

--- static/js/main.js
+++ static/js/main.js
@@ -4,7 +4,7 @@
 
 function getStylesheet() {
   var currentTime = new Date().getHours();
-  if ( currentTime <= 6 || currentTime >= 21) {
+  if ( currentTime <= 6 || currentTime >= 17) {
    document.write("<link rel='stylesheet' href='/static/css/night.css'>");
   }
 }
