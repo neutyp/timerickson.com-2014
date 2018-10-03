3f2b1c3032d58ede92f327cad63b738f3f7fdc01
Tim Erickson <tim@neutyp.com>
2018-10-02 17:13:15 -0700

    build

--- static/js/main.js
+++ static/js/main.js
@@ -4,7 +4,7 @@
 
 function getStylesheet() {
   var currentTime = new Date().getHours();
-  if (currentTime <= 6 || currentTime >= 17) {
+  if (currentTime <= 6 || currentTime >= 19) {
    document.write("<link rel='stylesheet' href='/static/css/night.css'>");
    var canvas = document.getElementById("day");
   canvas.id = "night";
